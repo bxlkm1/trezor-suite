745cee231168ff20fc7b99d2009d6a64f1720532
slowbackspace <slowbackspace@gmail.com>
2020-11-08 18:07:18 +0100

    fix: workaround for vtc, xlm coin logos in dark mode

--- packages/components/src/components/logos/CoinLogo/index.tsx
+++ packages/components/src/components/logos/CoinLogo/index.tsx
@@ -3,6 +3,7 @@ import { ReactSVG } from 'react-svg';
 import styled from 'styled-components';
 import { COINS } from './coins';
 import { CoinType } from '../../../support/types';
+import { useTheme } from '../../../utils';
 
 const SvgWrapper = styled.div<Omit<Props, 'symbol'>>`
     display: inline-block;
@@ -14,6 +15,11 @@ const SvgWrapper = styled.div<Omit<Props, 'symbol'>>`
     }
 `;
 
+const isLogoDark = (svg: SVGElement) => {
+    const fill = svg.getAttribute('fill');
+    return fill?.startsWith('#000') ?? false;
+};
+
 interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
     symbol: CoinType;
     className?: string;
@@ -21,6 +27,7 @@ interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
 }
 
 const CoinLogo = ({ symbol, className, size = 32, ...rest }: Props) => {
+    const theme = useTheme();
     return (
         <SvgWrapper className={className} size={size} {...rest}>
             <ReactSVG
@@ -28,6 +35,10 @@ const CoinLogo = ({ symbol, className, size = 32, ...rest }: Props) => {
                 beforeInjection={svg => {
                     svg.setAttribute('width', `${size}px`);
                     svg.setAttribute('height', `${size}px`);
+                    if (isLogoDark(svg)) {
+                        // set visible fill color for black colored logos (eg. XLM)
+                        svg.setAttribute('fill', theme.NEUE_TYPE_DARK_GREY);
+                    }
                 }}
                 loading={() => <span className="loading" />}
             />
